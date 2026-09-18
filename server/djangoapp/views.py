3595cfc1e22e7eb917c4e18759a715fdfb259f69
Daddyk5 <kenneth.gulmatico@hcdc.edu.ph>
2026-09-18 09:28:41 -0400

    Update full stack capstone project

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from .models import CarMake, CarModel
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_exempt
 import logging
@@ -60,6 +61,19 @@ def registration(request):
         return JsonResponse({"userName": username, "status": "Authenticated"})
     else:
         return JsonResponse({"userName": username, "error": "Already Registered"})
+
+#For CarModels
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
